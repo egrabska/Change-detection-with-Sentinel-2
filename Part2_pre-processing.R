cde717d9e69c9e4515f26121b130dae03c6d3442
Ewa Grabska <egrabska@PC-0001.ur.local>
2020-01-03 15:24:47 +0100

    s2translate

--- Part2_pre-processing.R
+++ Part2_pre-processing.R
@@ -1,8 +1,13 @@
-#sen2cor correction (from TOA to BOA reflectance)
+#sen2cor correction (from TOA to BOA reflectance) - for many directories (folders) 
 
-
-setwd("D:/02_Sentinel/paper3/get_data/Sentinel-2")
-dirs = list.dirs()
+dirs = list.dirs("path")
 dirs = dirs[grepl("*.SAFE$", dirs)]
 dirs_toa = dirs[grepl("*MSIL1C*", dirs)]
 lapply(dirs_toa[2], sen2cor)
+
+
+#producing one raster in .tiff format from Sentinel-2 bands 
+dirs = list.dirs("path")
+dirs = dirs[grepl("*.SAFE$", dirs)]
+
+lapply(dirs, s2_translate, format="GTiff")
