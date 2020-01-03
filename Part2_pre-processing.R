998063921c63a655afd044b4e4680553b3705ebb
Ewa Grabska <egrabska@PC-0001.ur.local>
2020-01-03 15:57:05 +0100

    indices etc

--- Part2_pre-processing.R
+++ Part2_pre-processing.R
@@ -1,4 +1,5 @@
-#sen2cor correction (from TOA to BOA reflectance) - for many directories (folders) 
+library(sen2r)
+#--1--sen2cor correction (from TOA to BOA reflectance) - for many directories (folders) 
 
 dirs = list.dirs("path")
 dirs = dirs[grepl("*.SAFE$", dirs)]
@@ -6,8 +7,21 @@ dirs_toa = dirs[grepl("*MSIL1C*", dirs)]
 lapply(dirs_toa[2], sen2cor)
 
 
-#producing one raster in .tiff format from Sentinel-2 bands 
+#--2--producing one raster in .tiff format from Sentinel-2 bands 
 dirs = list.dirs("path")
 dirs = dirs[grepl("*.SAFE$", dirs)]
 
 lapply(dirs, s2_translate, format="GTiff")
+
+#--3--calculating indices
+sort(list_indices("name", all=TRUE))
+
+#for single image
+s2_calcindices("path", 
+               indices = c("NDVI"), subdirs = FALSE,
+               parallel = TRUE)
+
+#for many images
+images= list.files(path = "", pattern ="*BOA_10.tif$*", full.names = TRUE)
+lapply(images, s2_calcindices, indices = c("MSI", "SLAVI"), subdirs = TRUE, parallel = TRUE)
+
