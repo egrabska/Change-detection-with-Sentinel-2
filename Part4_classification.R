0f792d01b8fee9fddeb9462b1938f952094d8f4a
Ewa Grabska <egrabska@PC-0001.ur.local>
2020-01-23 14:54:07 +0100

    corrections

--- Part4_classification.R
+++ Part4_classification.R
@@ -6,27 +6,29 @@ library(raster)
 library(caTools)
 library(kernlab)
 
-#Random Forest classification: x - path to classified raster
-RFclassification = function(x){ 
-  ref = shapefile("path to shape with reference areas in .shp")
+#Random Forest classification: x - path to classified raster, y -path to reference in .shp, z =- path to outputs
+RFclassification = function(x,y,z){ 
+  ref = shapefile(y)
   set.seed(5)
   split = sample.split(ref$Classname, 0.7) #spliting into training and validation sets based on "Classname"
   training_set = subset(ref, split == TRUE)
   validation_set = subset(ref, split == FALSE)
   rf_grid = expand.grid(mtry = c(1,2,3,5,10)) #setting different parameters of mtry
-  classification = superClass(stack(x), training_set, validation_set, responseCol = "Classname",
+  classification = superClass(stack(x), training_set, validation_set, responseCol = "spruce__53",
                     model = "rf", mode = "classification", tuneGrid = rf_grid, ntree =500)
-  writeRaster(classification$map, paste("path/", x, "_classification.tif", sep=""), overwrite = TRUE) #saving classification map
-  write.csv(classification$validation$performance$table, paste("path/", "table_", x, ".csv", sep="")) #saving confusion matrix
-  write.csv(classification$validation$performance$overall, paste("path/", "overall_", x, ".csv", sep="")) #saving accuracy metrics
+  writeRaster(classification$map, paste(z, "classification_RF.tif", sep=""), overwrite = TRUE) #saving classification map
+  write.csv(classification$validation$performance$table, paste(z, "table_RF.csv", sep="")) #saving confusion matrix
+  write.csv(classification$validation$performance$overall, paste(z, "overall_RF.csv", sep="")) #saving accuracy metrics
 }
+
+
 
 
 #by analogy - XGBoost classification and SVM below:
 
 
-klasyfikacjaXGB = function(x){
-  ref = shapefile("path")
+klasyfikacjaXGB = function(x,y,z){
+  ref = shapefile(y)
   set.seed(5)
   split = sample.split(ref$Classname, 0.7)
   training_set = subset(ref, split == TRUE)
@@ -35,15 +37,15 @@ klasyfikacjaXGB = function(x){
                            0.3573189, min_child_weight = 1, subsample = 0.9479397) #these are example hyperparamters, how to tune them - check out another script
   classification = superClass(stack(x),  training_set, validation_set, responseCol = "Classname",
                     model = "xgbTree", mode = "classification", tuneGrid = xgb_grid)
-  writeRaster(classification$map, paste("path/", x, "_classXGB.tif", sep=""), overwrite = TRUE)
-  write.csv(classification$validation$performance$table, paste("path/", "XGB_table_", x, ".csv", sep=""))
-  write.csv(classification$validation$performance$overall, paste("path/", "XGB_overall_", x, ".csv", sep=""))
+  writeRaster(classification$map, paste(z, "classification_XGB.tif", sep=""), overwrite = TRUE) #saving classification map
+  write.csv(classification$validation$performance$table, paste(z, "table_XGB.csv", sep="")) #saving confusion matrix
+  write.csv(classification$validation$performance$overall, paste(z, "overall_XGB.csv", sep="")) #saving accuracy metrics
 }
 
 
 
-klasyfikacjaSVM = function(x){
-  ref = shapefile("path")
+klasyfikacjaSVM = function(x,y,z){
+  ref = shapefile(y)
   set.seed(5)
   split = sample.split(ref$Classname, 0.7)
   training_set = subset(ref, split == TRUE)
@@ -51,7 +53,7 @@ klasyfikacjaSVM = function(x){
   SVM_grid = expand.grid(degree = 2, scale = 1.8996, C = 9.243558) #these are example hyperparamters, how to tune them - check out another script
   classification = superClass(stack(x),  training_set, validation_set, responseCol = "Classname",
                     model = "svmPoly", mode = "classification", tuneGrid = SVM_grid)
-  writeRaster(classification$map, paste("path/", x, "_classSVM.tif", sep=""), overwrite = TRUE)
-  write.csv(classification$validation$performance$table, paste("path/", "SVM_table_", x, ".csv", sep=""))
-  write.csv(classification$validation$performance$overall, paste("path/", "SVM_overall_", x, ".csv", sep=""))
+  writeRaster(classification$map, paste(z, "classification_SVM.tif", sep=""), overwrite = TRUE) #saving classification map
+  write.csv(classification$validation$performance$table, paste(z, "table_SVM.csv", sep="")) #saving confusion matrix
+  write.csv(classification$validation$performance$overall, paste(z, "overall_SVM.csv", sep="")) #saving accuracy metrics
 }
